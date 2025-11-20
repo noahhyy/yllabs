70afeb85fef39d8f1a693d2d6224766dd510e64a
noahhyy <126304197+noahhyy@users.noreply.github.com>
2025-11-19 21:35:10 -0500

    Delete staff-dashboard directory

--- staff-dashboard/staff/components/Navbar.tsx
+++
@@ -1,8 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="bg-blue-800 p-4 text-white flex justify-between">
-      <h1 className="font-bold">Pulse Labs Staff Dashboard</h1>
-      <a href="/logout" className="hover:underline">Logout</a>
-    </nav>
-  );
-}
